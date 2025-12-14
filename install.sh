9c1c43f5fccaa9e3e583697af762a8889c21b416
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-12-14 06:36:04 +0400

    feat: turn off natural scrolling

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ defaults write com.apple.dock autohide -bool true
 defaults write com.apple.Dock autohide -bool true
 defaults write com.apple.Dock autohide-delay -float 1000
 defaults write com.apple.universalaccess reduceMotion -bool true
+defaults write -g com.apple.swipescrolldirection -bool false
 
 
 killall SystemUIServer
